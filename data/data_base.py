4a313b8a765f662fcf46889537c7fb848963811e
Mikhail Belskiy <mikl.belskiy@yandex.ru>
2024-12-07 00:18:59 +0300

    Убрал лишние таблицы в models Добавил функции записи в БД файл data_base

--- data/data_base.py
+++ data/data_base.py
@@ -1,20 +1,53 @@
+from sqlalchemy import Engine
 from sqlalchemy.orm import sessionmaker
-from models import Users, Candidates, Favourites, create_tables
+from models import Users, Favourites, create_tables
 import sqlalchemy
 from sqlalchemy.exc import SQLAlchemyError
 
 DSN = "postgresql://postgres:Mb20041995@localhost:5432/VK_Bot_team"
-engine = sqlalchemy.create_engine(DSN)
+engine: Engine = sqlalchemy.create_engine(DSN)
 
 try:
     create_tables(engine)  # Создаём таблицы
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Здесь можно выполнять операции с сессией
 
-    # Закрытие сессии
 except SQLAlchemyError as e:
     print(f"Ошибка базы данных: {e}")
 finally:
     session.close()
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def add_user(vk_user_id): #создание пользователя
+    user = Users(vk_user_id=vk_user_id)
+    session.add(user)
+    try:
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"Ошибка при добавлении пользователя: {e}")
+
+def add_favourite(vk_user_id, favourite_id, chosen=True): #добавление в избр-е
+    favourite = Favourites(vk_user_id=vk_user_id, favourite_id=favourite_id, chosen=chosen)
+    session.add(favourite)
+    try:
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"Ошибка при добавлении в избранное: {e}")
+
+def add_blacklist(vk_user_id, favourite_id, chosen=False): # добавление в ЧС
+    blacklist = Favourites(vk_user_id=vk_user_id, favourite_id=favourite_id, chosen=chosen)
+    session.add(blacklist)
+    try:
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"Ошибка при добавлении в черный список: {e}")
+session.close()
+
+
--- data/models.py
+++ data/models.py
@@ -9,33 +9,18 @@ class Users(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     vk_user_id = sq.Column(sq.Integer, unique=True, nullable=False)
-    age = sq.Column(sq.Integer, nullable=False)
-    gender = sq.Column(sq.String(length=10), nullable=False)
-    city = sq.Column(sq.String(length=30), nullable=False)
-
-class Candidates(Base):
-    __tablename__ = 'candidates'
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    user_id = sq.Column(sq.Integer, sq.ForeignKey('users.id'))
-    vk_id = sq.Column(sq.Integer, unique=True, nullable=False)
-    full_name = sq.Column(sq.String(length=100), nullable=False)
-    link_vk = sq.Column(sq.String(length=400), nullable=False)
-    photo_1 = sq.Column(sq.String(length=400), nullable=False)
-    photo_2 = sq.Column(sq.String(length=400), nullable=True)
-    photo_3 = sq.Column(sq.String(length=400), nullable=True)
 
 class Favourites(Base):
     __tablename__= 'favourites'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    favourite_id = sq.Column(sq.Integer, sq.ForeignKey('candidates.id'))
-    user_id = sq.Column (sq.Integer, sq.ForeignKey('users.id'))
+    vk_user_id = sq.Column (sq.Integer, sq.ForeignKey('users.vk_user_id'))
+    favourite_id = sq.Column(sq.Integer,  unique=True)
     chosen = sq.Column(sq.Boolean, nullable=True)
 
 def create_tables(engine):
     try:
-        Base.metadata.drop_all(engine) # Удалить все таблицы
+        # Base.metadata.drop_all(engine) # Удалить все таблицы
         Base.metadata.create_all(engine)
         print("Таблицы успешно созданы")
     except SQLAlchemyError as e:
